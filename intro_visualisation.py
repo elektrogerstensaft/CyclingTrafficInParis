5b3151f3766f8a93e261b102311b457f107c7420
soydberger <152404094+soydberger@users.noreply.github.com>
2023-12-06 17:08:28 +0100

    Update intro_visualisation.py
    Only translated the column names/terms:
    French -> English

--- intro_visualisation.py
+++ intro_visualisation.py
@@ -1,22 +1,25 @@
 import pandas as pd
 import numpy as np
-df = pd.read_csv("CyclingTrafficInParis.csv")
+df = pd.read_csv("CyclingTrafficInParis_eng.csv")
 
-df["Date et heure de comptage"] = pd.to_datetime(df["Date et heure de comptage"], utc= True)
-df["Date d'installation du site de comptage"] = pd.to_datetime(df["Date d'installation du site de comptage"])
-#print(df.info())
-#creating the columns weekday, week of year and hour of day
-# month of year already present as "mois_annee_comptage" 
-df["weekday_of_count"] = df["Date et heure de comptage"].dt.dayofweek
-df["week_year"] = df["Date et heure de comptage"].dt.year.astype(str) +"-"+ df["Date et heure de comptage"].dt.isocalendar().week.astype(str)
-df["hour_of_day"] = df["Date et heure de comptage"].dt.hour
+df["Date and time of count"] = pd.to_datetime(df["Date and time of count"], utc= True)
+df["Counting site installation date"] = pd.to_datetime(df["Counting site installation date"])
+#print(df_en.info())
+
+# creating the columns weekday, week of year and hour of day
+# month of year already present as "Month and year of count"
+
+df["weekday_of_count"] = df["Date and time of count"].dt.dayofweek
+df["week_year"] = df["Date and time of count"].dt.year.astype(str) +"-"+ df["Date and time of count"].dt.isocalendar().week.astype(str)
+df["hour_of_day"] = df["Date and time of count"].dt.hour
 print(df["hour_of_day"].value_counts())
 
-#I want to add a direction column, but this is too complicated right now, as the directions is encoded with different options in the column Nom du compteur"
+# I want to add a direction column, but this is too complicated right now,
+# as the directions is encoded with different options in the column Counter name"
 
 import plotly.express as px
 import matplotlib.pyplot as plt
 
-fig = px.box(df, y ="Comptage horaire", x = "mois_annee_comptage")
+fig = px.box(df, y ="Hourly count", x = "Month and year of count")
 
 fig.show()
