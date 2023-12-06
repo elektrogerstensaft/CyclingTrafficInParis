87477c7febac6ce22449cfb544ba0d0a067d6afc
elektrogerstensaft <47448645+elektrogerstensaft@users.noreply.github.com>
2023-12-06 18:06:34 +0100

    Added Histogram visualisation

--- intro_visualisation.py
+++ intro_visualisation.py
@@ -2,24 +2,41 @@ import pandas as pd
 import numpy as np
 df = pd.read_csv("CyclingTrafficInParis_eng.csv")
 
+"""
+The columns in this file are:
+Counter ID
+Counter name
+Counting site ID
+Counting site name
+Hourly count
+Date and time of count
+Counting site installation date
+Geographic coordinates
+Technical counter ID
+Month and year of count
+"""
+
 df["Date and time of count"] = pd.to_datetime(df["Date and time of count"], utc= True)
 df["Counting site installation date"] = pd.to_datetime(df["Counting site installation date"])
-#print(df_en.info())
 
-# creating the columns weekday, week of year and hour of day
-# month of year already present as "Month and year of count"
-
+#creating the columns weekday, week of year and hour of day
+# month of year already present as "Month and year of count" 
 df["weekday_of_count"] = df["Date and time of count"].dt.dayofweek
 df["week_year"] = df["Date and time of count"].dt.year.astype(str) +"-"+ df["Date and time of count"].dt.isocalendar().week.astype(str)
 df["hour_of_day"] = df["Date and time of count"].dt.hour
-print(df["hour_of_day"].value_counts())
 
-# I want to add a direction column, but this is too complicated right now,
-# as the directions is encoded with different options in the column Counter name"
+#I want to add a direction column, but this is too complicated right now, as the directions is encoded with different options in the column "Counting site name""
 
 import plotly.express as px
 import matplotlib.pyplot as plt
 
+#Boxplot of all Hourly counts
 fig = px.box(df, y ="Hourly count", x = "Month and year of count")
+fig.show()
 
+#Histogram of all Hourly count values, logarithmic y axis
+fig = px.histogram(df["Hourly count"],
+                    x="Hourly count",
+                    log_y=True,
+                    title='Histogram of Hourly counts',)
 fig.show()
