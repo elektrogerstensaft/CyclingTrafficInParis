7ca72896b88c094da895c7a94dea3c9272c99061
soydberger <152404094+soydberger@users.noreply.github.com>
2023-12-06 12:41:01 +0100

    First data preparation
    Contains the following things:
    1. Dropping redundant columns
    2. Delete (partly non existing data before October 2022)
    3. Translate columns into english
    4. Exporting the new .cvs as CyclingTrafficInParis_eng.csv

---
+++ FirstPreparation.py
@@ -0,0 +1,47 @@
+import pandas as pd
+
+df = pd.read_csv("comptage-velo-donnees-compteurs.csv", sep = ";")
+print(df.info())
+df["Date et heure de comptage"] = pd.to_datetime(df["Date et heure de comptage"])
+
+"""
+---
+For a smaller .csv file size we exclude redundant columns:
+
+Lien vers photo du site de comptage
+ID Photos
+test_lien_vers_photos_du_site_de_comptage_.value_counts
+id_photo_1
+url_sites
+type_dimage
+---
+
+"""
+
+# Resizing the df
+drop_cols = ["Lien vers photo du site de comptage", "ID Photos", "test_lien_vers_photos_du_site_de_comptage_",
+             "id_photo_1", "url_sites", "type_dimage"]
+df.drop(columns=drop_cols, inplace=True)
+
+# erasing data before October 2022
+date_threshold = pd.to_datetime('2022-10-01 00:00:00+01:00', utc=True)
+df = df[df["Date et heure de comptage"] >= date_threshold]
+
+# translate columns
+translation = {'Identifiant du compteur': 'Counter ID',
+               'Nom du compteur': 'Counter name',
+               'Identifiant du site de comptage': 'Counting site ID',
+               'Nom du site de comptage': 'Counting site name',
+               'Comptage horaire': 'Hourly count',
+               'Date et heure de comptage': 'Date and time of count',
+               'Date d\'installation du site de comptage': 'Counting site installation date',
+               'Coordonnées géographiques': 'Geographic coordinates',
+               'Identifiant technique compteur': 'Technical counter ID',
+               'mois_annee_comptage': 'Month and year of count'}
+
+
+df_en = df.copy()
+df_en.columns = [translation[col_name] for col_name in df_en.columns]
+
+# export the df as a separate new one
+df_en.to_csv("CyclingTrafficInParis_eng.csv", index=False)
