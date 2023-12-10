c97fe9e4d6398014d8d3c4c4fcdfad48dafcc3de
elektrogerstensaft <47448645+elektrogerstensaft@users.noreply.github.com>
2023-12-10 22:05:08 +0100

    added heatmaps for daytimes and days of months

--- intro_visualisation.py
+++ intro_visualisation.py
@@ -23,6 +23,8 @@ df["Counting site installation date"] = pd.to_datetime(df["Counting site install
 df["weekday_of_count"] = df["Date and time of count"].dt.dayofweek
 df["week_year"] = df["Date and time of count"].dt.year.astype(str) +"-"+ df["Date and time of count"].dt.isocalendar().week.astype(str)
 df["hour_of_day"] = df["Date and time of count"].dt.hour
+df["day"] = df["Date and time of count"].dt.day
+
 
 #I want to add a direction column, but this is too complicated right now, as the directions is encoded with different options in the column "Counting site name""
 
@@ -39,3 +41,47 @@ fig = px.histogram(df["Hourly count"],
                     log_y=True,
                     title='Histogram of Hourly counts',)
 fig.show()
+
+
+# Heatmap of days with most traffic
+grouped_multiple = df.groupby(['Month and year of count', 'day']).agg({'Hourly count': ["mean", "median"]})
+grouped_multiple.columns = ["Hourly_count_mean", "Hourly_count_median"]
+grouped_multiple = grouped_multiple.reset_index()
+
+import plotly.graph_objects as go
+fig = go.Figure(
+    data = go.Heatmap(
+        z = grouped_multiple["Hourly_count_median"],
+        x = grouped_multiple["Month and year of count"],
+        y = grouped_multiple["day"]
+    )
+)
+fig.update_xaxes(title = "Month and year")
+fig.update_yaxes(title = "Day of month")
+fig.update_layout(
+    title="Heatmap of days with most bicycle traffic"
+)
+fig.show()
+
+# Heatmap of days and months with most traffic
+grouped_multiple = df.groupby(["hour_of_day", 'weekday_of_count']).agg({'Hourly count': ["mean", "median","sum"]})
+grouped_multiple.columns = ["Hourly_count_mean", "Hourly_count_median","Hourly_count_sum"]
+grouped_multiple = grouped_multiple.reset_index()
+
+import plotly.graph_objects as go
+fig = go.Figure(
+    data = go.Heatmap(
+        z = grouped_multiple["Hourly_count_sum"],
+        x = grouped_multiple["hour_of_day"],
+        y = grouped_multiple["weekday_of_count"]
+    )
+)
+fig.update_xaxes(title = "Hour of day")
+fig.update_yaxes(title = "Weekday")
+fig.update_layout(
+    title="Heatmap of daytimes per weekday with most bicycle traffic"
+)
+fig.show()
+
+
+
