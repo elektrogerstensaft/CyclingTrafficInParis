4ff67abdc3ec257baf1b531ad4b8fe488200ea44
soydberger <152404094+soydberger@users.noreply.github.com>
2024-01-02 15:08:31 +0100

    Restructuring RandomForestRegression.py

--- RandomForestRegression.py
+++ RandomForestRegression.py
@@ -5,10 +5,10 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error
 import matplotlib.pyplot as plt
 
 # read data
-X_train = pd.read_csv("X_train.csv")
-X_test = pd.read_csv("X_test.csv")
-y_train = pd.read_csv("y_train.csv")
-y_test = pd.read_csv("y_test.csv")
+X_train = pd.read_csv("../X_train.csv")
+X_test = pd.read_csv("../X_test.csv")
+y_train = pd.read_csv("../y_train.csv")
+y_test = pd.read_csv("../y_test.csv")
 
 # create + train Random Forest Regression
 rfr = RandomForestRegressor(n_estimators=100, random_state=42)
@@ -17,16 +17,6 @@ rfr.fit(X_train, np.ravel(y_train))
 # display the results of the score
 print("Trainings R^2 score:", rfr.score(X_train, np.ravel(y_train)))
 print("Test R^2 score:", rfr.score(X_test, np.ravel(y_test)))
-
-# visualise predictions
-fig = plt.figure(figsize=(8, 8))
-pred_test = rfr.predict(X_test)
-plt.scatter(pred_test, y_test, c="#8DA0CB")
-plt.plot((y_test.min(), y_test.max()), (y_test.min(), y_test.max()), color="red", alpha=0.6)
-plt.xlabel("Predicted values")
-plt.ylabel("True values")
-plt.title("Random Forest Regression for bike countings")
-plt.show()
 
 # metrics
 y_pred_train_rfr = rfr.predict(X_train)
@@ -50,3 +40,13 @@ data_rf = {"MAE Train data": [mae_train_rfr],
 
 df_rf = pd.DataFrame(data_rf, index=["Random Forest every variable"])
 print(df_rf)
+
+# visualise predictions
+fig = plt.figure(figsize=(8, 8))
+pred_test = rfr.predict(X_test)
+plt.scatter(pred_test, y_test, c="teal")
+plt.plot((y_test.min(), y_test.max()), (y_test.min(), y_test.max()), color="red", alpha=0.6)
+plt.xlabel("Predicted values")
+plt.ylabel("True values")
+plt.title("Random Forest Regression for bike countings")
+plt.show()
