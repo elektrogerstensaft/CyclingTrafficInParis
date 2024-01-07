7c81f5b9d11e6444a05057e941d30a2c552e8c7b
soydberger <152404094+soydberger@users.noreply.github.com>
2024-01-07 16:31:05 +0100

    Update RandomForestRegression.py

--- RandomForestRegression.py
+++ RandomForestRegression.py
@@ -1,52 +1,82 @@
 import pandas as pd
-import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, mean_absolute_error
-import matplotlib.pyplot as plt
 
 # read data
-X_train = pd.read_csv("../X_train.csv")
-X_test = pd.read_csv("../X_test.csv")
-y_train = pd.read_csv("../y_train.csv")
-y_test = pd.read_csv("../y_test.csv")
+X_train = pd.read_csv("X_train.csv")
+X_test = pd.read_csv("X_test.csv")
+y_train = pd.read_csv("y_train.csv")
+y_test = pd.read_csv("y_test.csv")
 
-# create + train Random Forest Regression
-rfr = RandomForestRegressor(n_estimators=100, random_state=42)
-rfr.fit(X_train, np.ravel(y_train))
+## create + train Random Forest Regressions
+# model 1
+rfr_1 = RandomForestRegressor(n_estimators=100, random_state=42)
+rfr_1.fit(X_train, y_train.values.ravel())
+
+# model 2
+rfr_2 = RandomForestRegressor(n_estimators=85, max_depth=4, random_state=42)
+rfr_2.fit(X_train, y_train.values.ravel())
 
 # display the results of the score
-print("Trainings R^2 score:", rfr.score(X_train, np.ravel(y_train)))
-print("Test R^2 score:", rfr.score(X_test, np.ravel(y_test)))
+print("Train score with all variables (First Model):", rfr_1.score(X_train, y_train))
+print("Test score with all variables (First Model):", rfr_1.score(X_test, y_test))
+print("Train score with all variables (Second Model):", rfr_2.score(X_train, y_train))
+print("Test score with all variables (Second Model):", rfr_2.score(X_test, y_test))
 
-# metrics
-y_pred_train_rfr = rfr.predict(X_train)
-y_pred_test_rfr = rfr.predict(X_test)
+# predictions
+y_pred_rfr_1 = rfr_1.predict(X_test)
+y_pred_train_rfr_1 = rfr_1.predict(X_train)
 
-mae_train_rfr = mean_absolute_error(y_train, y_pred_train_rfr)
-mse_train_rfr = mean_squared_error(y_train, y_pred_train_rfr)
-rmse_train_rfr = np.sqrt(mse_train_rfr)
+y_pred_rfr_2 = rfr_2.predict(X_test)
+y_pred_train_rfr_2 = rfr_2.predict(X_train)
 
-mae_test_rfr = mean_absolute_error(y_test, y_pred_test_rfr)
-mse_test_rfr = mean_squared_error(y_test, y_pred_test_rfr)
-rmse_test_rfr = np.sqrt(mse_test_rfr)
+## metrics
+# Training set metrics
+mae_rfr_1_train = mean_absolute_error(y_train, y_pred_train_rfr_1)
+mse_rfr_1_train = mean_squared_error(y_train, y_pred_train_rfr_1, squared=True)
+rmse_rfr_1_train = mean_squared_error(y_train, y_pred_train_rfr_1, squared=False)
+
+mae_rfr_2_train = mean_absolute_error(y_train, y_pred_train_rfr_2)
+mse_rfr_2_train = mean_squared_error(y_train, y_pred_train_rfr_2, squared=True)
+rmse_rfr_2_train = mean_squared_error(y_train, y_pred_train_rfr_2, squared=False)
+
+# Test set metrics
+mae_rfr_1_test = mean_absolute_error(y_test, y_pred_rfr_1)
+mse_rfr_1_test = mean_squared_error(y_test, y_pred_rfr_1, squared=True)
+rmse_rfr_1_test = mean_squared_error(y_test, y_pred_rfr_1, squared=False)
+
+mae_rfr_2_test = mean_absolute_error(y_test, y_pred_rfr_2)
+mse_rfr_2_test = mean_squared_error(y_test, y_pred_rfr_2, squared=True)
+rmse_rfr_2_test = mean_squared_error(y_test, y_pred_rfr_2, squared=False)
 
 # save results 
-data_rf = {"MAE Train data": [mae_train_rfr],
-           "MAE Test data": [mae_test_rfr],
-           "MSE Train data": [mse_train_rfr],
-           "MSE Test data": [mse_test_rfr],
-           "RMSE Train data": [rmse_train_rfr],
-           "RMSE Test data": [rmse_test_rfr]}
+data = {"MAE Train data": [mae_rfr_1_train, mae_rfr_2_train],
+        "MAE Test data": [mae_rfr_1_test, mae_rfr_2_test],
+        "MSE Train data": [mae_rfr_1_train, mae_rfr_2_train],
+        "MSE Test data": [mae_rfr_1_test, mae_rfr_2_test],
+        "RMSE Train data": [mae_rfr_1_train, mae_rfr_2_train],
+        "RMSE Test data": [mae_rfr_1_test, mae_rfr_2_test]}
 
-df_rf = pd.DataFrame(data_rf, index=["Random Forest every variable"])
-print(df_rf)
+df = pd.DataFrame(data, index=["First Model", "Second Model"])
+pd.set_option("display.max_columns", None)
+print(df.head())
 
-# visualise predictions
-fig = plt.figure(figsize=(8, 8))
-pred_test = rfr.predict(X_test)
-plt.scatter(pred_test, y_test, c="teal")
+## visualisations
+# model 1
+plt.figure(figsize=(8, 8))
+plt.scatter(y_pred_rfr_1, y_test, c="teal")
 plt.plot((y_test.min(), y_test.max()), (y_test.min(), y_test.max()), color="red", alpha=0.6)
 plt.xlabel("Predicted values")
 plt.ylabel("True values")
-plt.title("Random Forest Regression for bike countings")
+plt.title("Random Forest Regression (Model 1) for bike countings")
+plt.show()
+
+# model 2
+plt.figure(figsize=(8, 8))
+plt.scatter(y_pred_rfr_2, y_test, c="orange")
+plt.plot((y_test.min(), y_test.max()), (y_test.min(), y_test.max()), color="red", alpha=0.6)
+plt.xlabel("Predicted values")
+plt.ylabel("True values")
+plt.title("Random Forest Regression (Model 2) for bike countings")
 plt.show()
