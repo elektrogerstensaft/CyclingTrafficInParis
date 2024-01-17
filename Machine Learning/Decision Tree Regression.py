597fb36d744f1454b3eeca47fbe6581e35aa8a6e
soydberger <152404094+soydberger@users.noreply.github.com>
2024-01-17 17:55:37 +0100

    Update Decision Tree Regression.py
    adding short comments

--- Machine Learning/Decision Tree Regression.py
+++ Machine Learning/Decision Tree Regression.py
@@ -1,36 +1,37 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeRegressor 
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 
+# read data
 X_train = pd.read_csv("X_train_Weather.csv")
 X_test = pd.read_csv("X_test_Weather.csv")
 y_train = pd.read_csv("y_train_Weather.csv")
 y_test = pd.read_csv("y_test_Weather.csv")
 
-from sklearn.tree import DecisionTreeRegressor 
+## create + train models
+# model 1
 regressor = DecisionTreeRegressor(random_state=42, min_samples_leaf = 25 ) 
 regressor.fit(X_train, y_train)
 
+# model 2
 regressor_random_splitter = DecisionTreeRegressor(random_state=42, min_samples_leaf= 20) 
 regressor_random_splitter.fit(X_train, y_train)
 
+# display the results of the score
 print("Train score with all variables", regressor.score(X_train, y_train))
 print("Test score with all variables", regressor.score(X_test, y_test))
 print("Train score with all variables", regressor_random_splitter.score(X_train, y_train))
 print("Test score with all variables", regressor_random_splitter.score(X_test, y_test))
 
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import mean_absolute_error
-
-#Metrics of the model
-
+# predictions
 y_pred_decision_tree = regressor.predict(X_test)
 y_pred_train_decision_tree = regressor.predict(X_train)
 
 y_pred_decision_tree_splitter = regressor_random_splitter.predict(X_test)
 y_pred_train_decision_tree_splitter = regressor_random_splitter.predict(X_train)
 
-# Metrics
-
+## metrics
 # Training set
 mae_decision_tree_train = mean_absolute_error(y_train, y_pred_train_decision_tree)
 mse_decision_tree_train = mean_squared_error(y_train, y_pred_train_decision_tree, squared=True)
@@ -49,20 +50,21 @@ mae_decision_tree_test_splitter = mean_absolute_error(y_test, y_pred_decision_tr
 mse_decision_tree_test_splitter = mean_squared_error(y_test, y_pred_decision_tree_splitter, squared=True)
 rmse_decision_tree_test_splitter = mean_squared_error(y_test, y_pred_decision_tree_splitter, squared=False)
 
-data = {
-    'MAE train': [mae_decision_tree_train, mae_decision_tree_train_splitter],
-    'MAE test': [mae_decision_tree_test, mae_decision_tree_test_splitter],
-    'MSE train': [mse_decision_tree_train, mse_decision_tree_train_splitter],
-    'MSE test': [mse_decision_tree_test, mse_decision_tree_test_splitter],
-    'RMSE train': [rmse_decision_tree_train, rmse_decision_tree_train_splitter],
-    'RMSE test': [rmse_decision_tree_test, mse_decision_tree_test_splitter]
-}
+# save results
+data = {'MAE train': [mae_decision_tree_train, mae_decision_tree_train_splitter],
+        'MAE test': [mae_decision_tree_test, mae_decision_tree_test_splitter],
+        'MSE train': [mse_decision_tree_train, mse_decision_tree_train_splitter],
+        'MSE test': [mse_decision_tree_test, mse_decision_tree_test_splitter],
+        'RMSE train': [rmse_decision_tree_train, rmse_decision_tree_train_splitter],
+        'RMSE test': [rmse_decision_tree_test, mse_decision_tree_test_splitter]
+       }
 df = pd.DataFrame(data, index=["First Model", "Second Model"])
 print(df.head())
 
 
 """
-#Hyper parameter tuning
+This part was used for a hyper parameter tuning. It still can be used but leads to unstable results with very long calculating times!
+
 parameters={"max_depth" : [1,3,5,7,9,11,13],
             "max_features":["log2","sqrt",None],
             "max_leaf_nodes":[None,15,30,45,60,75,90],
